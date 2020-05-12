1163f7276ee60bcf941cff856836426bc3d2f790
Marie Antons <chefantons@gmail.com>
2020-05-11 23:41:56 -0500

    added bird and animals

---
+++ MarieZoo/Animals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace MarieZoo
+{
+
+
+	/// <summary>
+	/// Summary description for Class1
+	/// </summary>
+	public  abstract class Animals
+	{
+
+		public virtual bool InZoo();
+
+		public abstract void Eat();
+
+		public abstract string Sound();
+
+	}
+}
