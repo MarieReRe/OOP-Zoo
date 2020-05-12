16ba971d5ebc0df347647a11f5aafe910cd08b83
Marie Antons <chefantons@gmail.com>
2020-05-11 22:16:43 -0500

    Scaffolding

---
+++ MarieZoo/MarieZoo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MarieZoo
+{
+    /*            My Zoo Needs:
+         [x] 1.  Have at least 3 different abstract classes
+         [x] 2. Have at least 3 layers of inheritance
+         [x] 3.Have at least 5 concrete animals
+         [x] 4. Have at least 2 abstract methods (make sure you override them)
+         [x] 5. Have at least 2 abstract Properties (make sure you override them
+         [x] 6.Have at least 2 virtual methods (make sure you override at least one of them)
+         [x] 7. Have at least 2 virtual properties (make sure you override at least one of them)
+         [x] 8. Your (digital) drawing of your zoo diagram should be clearly labeled
+     */
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
